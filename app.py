ec3a1fbf5d1a314c5479bde60b94ac1a911ea984
yuyang-sun-2019 <81420946+yuyang-sun-2019@users.noreply.github.com>
2023-03-29 18:38:06 +0800

    comment

--- app.py
+++ app.py
@@ -25,44 +25,45 @@ def hello():
 
 @app.route("/<name>")
 def hello_name(name):
-    try:
-        BASE_URL = "https://web.whatsapp.com/"
-        CHAT_URL = "https://web.whatsapp.com/send?phone={phone}&text&type=phone_number&app_absent=1"
+    print(name)
+    # try:
+    #     BASE_URL = "https://web.whatsapp.com/"
+    #     CHAT_URL = "https://web.whatsapp.com/send?phone={phone}&text&type=phone_number&app_absent=1"
 
-        chrome_options = Options()
-        chrome_options.add_argument("start-maximized")
-        user_data_dir = ''.join(random.choices(string.ascii_letters, k=8))
-        chrome_options.add_argument("--user-data-dir=/tmp/chrome-data/" + user_data_dir)
-        chrome_options.add_argument("--incognito")
+    #     chrome_options = Options()
+    #     chrome_options.add_argument("start-maximized")
+    #     user_data_dir = ''.join(random.choices(string.ascii_letters, k=8))
+    #     chrome_options.add_argument("--user-data-dir=/tmp/chrome-data/" + user_data_dir)
+    #     chrome_options.add_argument("--incognito")
 
-        browser = webdriver.Chrome(ChromeDriverManager().install(),  options=chrome_options,)
+    #     browser = webdriver.Chrome(ChromeDriverManager().install(),  options=chrome_options,)
 
-        browser.get(BASE_URL)
-        browser.maximize_window()
-
-
-        phone = "83193088"   #change to sponsor number 86687676
-        message = 'I need service for '+ name
+    #     browser.get(BASE_URL)
+    #     browser.maximize_window()
 
 
-        browser.get(CHAT_URL.format(phone=phone))
-        time.sleep(3)
+    #     phone = "83193088"   #change to sponsor number 86687676
+    #     message = 'I need service for '+ name
 
 
-        inp_xpath = (
-            '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]'
-        )
-        input_box = WebDriverWait(browser, 60).until(
-            expected_conditions.presence_of_element_located((By.XPATH, inp_xpath))
-        )
-        input_box.send_keys(message)
-        input_box.send_keys(Keys.ENTER)
+    #     browser.get(CHAT_URL.format(phone=phone))
+    #     time.sleep(3)
 
-        time.sleep(10)
-        driver.quit()
-    except:
-        pass
-    return "Hello there, we have whatsapp your query to our Project V2D department"
+
+    #     inp_xpath = (
+    #         '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]'
+    #     )
+    #     input_box = WebDriverWait(browser, 60).until(
+    #         expected_conditions.presence_of_element_located((By.XPATH, inp_xpath))
+    #     )
+    #     input_box.send_keys(message)
+    #     input_box.send_keys(Keys.ENTER)
+
+    #     time.sleep(10)
+    #     driver.quit()
+    # except:
+    #     pass
+    # return "Hello there, we have whatsapp your query to our Project V2D department"
 
 
 
